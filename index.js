9bac5af010aefb239dc466547d10d01551bd0638
yinm <yinm.cminor9@gmail.com>
2018-03-28 09:39:44 +0900

    Add serialize()

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 
 exports.parse = parse
+exports.serialize = serialize
 
 /**
  * Module variables.
@@ -74,6 +75,107 @@ function parse(str, options) {
   }
 
   return obj
+}
+
+/**
+ * Serialize data into a cookie header.
+ *
+ * Serialize a name value pair into a cookie string suitable for
+ * http headers. An optional options object specified cookie parameters.
+ *
+ * serialize('foo', 'bar', { httpOnly: true })
+ *  => "foo=bar; httpOnly"
+ *
+ *  @param {string} name
+ *  @param {string} val
+ *  @param {object} [options]
+ *  @return {string}
+ *  @public
+ */
+
+function serialize(name, val, options) {
+  const
+    opt = options || {},
+    enc = opt.encode || encode
+
+  if (typeof enc !== 'function') {
+    throw new TypeError('option encode is invalid')
+  }
+
+  if (!fieldContentRegExp.test(name)) {
+    throw new TypeError('argument name is invalid')
+  }
+
+  const value = enc(val)
+
+  if (value && !fieldContentRegExp.test(value)) {
+    throw new TypeError('argument val is invalid')
+  }
+
+  let str = `${name}=${value}`
+
+  if (opt.maxAge) {
+    const maxAge = opt.maxAge - 0
+    if (isNaN(maxAge)) {
+      throw new Error('maxAge should be a Number')
+    }
+
+    str += `; Max-Age=${Math.floor(maxAge)}`
+  }
+
+  if (opt.domain) {
+    if (!fieldContentRegExp.test(opt.domain)) {
+      throw new TypeError('option domain is invalid')
+    }
+
+    str += `; Domain=${opt.domain}`
+  }
+
+  if (opt.path) {
+    if (!fieldContentRegExp.test(opt.path)) {
+      throw new TypeError('option path is invalid')
+    }
+
+    str += `; Path=${opt.path}`
+  }
+
+  if (opt.expires) {
+    if (typeof opt.expires.toUTCString !== 'function') {
+      throw new TypeError('option expires is invalid')
+    }
+
+    str += `; Expires=${opt.expires.toUTCString()}`
+  }
+
+  if (opt.httpOnly) {
+    str += '; HttpOnly'
+  }
+
+  if (opt.secure) {
+    str += '; Secure'
+  }
+
+  if (opt.sameSite) {
+    const sameSite = typeof opt.sameSite === 'string'
+      ? opt.sameSite.toLowerCase()
+      : opt.sameSite
+
+    switch (sameSite) {
+      case true:
+        str += '; SameSite=Strict'
+        break
+      case 'lax':
+        str += '; SameSite=Lax'
+        break
+      case 'strict':
+        str += '; SameSite=Strict'
+        break
+      default:
+        throw new TypeError('option sameSite is invalid')
+    }
+  }
+
+  return str
 }
 
 /**
